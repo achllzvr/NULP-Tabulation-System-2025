61dfc62975768c695ee6b5164180530920e070c5
Achilles Vonn Rabina <140421417+achllzvr@users.noreply.github.com>
2025-10-02 17:15:10 +0800

    feat: Update navigation to replace Leaderboard with Results and streamline sidebar links

--- partials/sidebar_admin.php
+++ partials/sidebar_admin.php
@@ -35,11 +35,7 @@ $navItems = [
         'href' => $prefix . 'live_control.php',
         'icon' => 'M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z'
     ],
-    [
-        'label' => 'Leaderboard',
-        'href' => $prefix . 'leaderboard.php',
-        'icon' => 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z'
-    ],
+  // Leaderboard removed; centralized under Results / public pages
   [
     'label' => 'Results',
     'href' => $prefix . 'results.php',
